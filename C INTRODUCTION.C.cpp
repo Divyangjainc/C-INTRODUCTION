1612b4e0a95ce466558eb84922da6f0d6d03ba78
Divyangjainc <113012854+Divyangjainc@users.noreply.github.com>
2022-11-10 13:27:36 +0530

    Add files via upload

---
+++ C INTRODUCTION.C.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h> 
+ int main()  
+    {
+     printf("Name   : DIVYANG JAIN\n"); 
+     printf("DOB    : NOVEMBER 30, 2006\n"); 
+     printf("Age    :  17 years\n");
+     printf("ADDRESS : 21 NEW JAIN MILAN Society near Amraiwadi Surelia estate Ahmedabad 380026\n"); 
+     return(0);
+    }
